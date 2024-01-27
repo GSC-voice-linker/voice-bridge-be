1e835c54641b2b3eac8e7cf7c34e09123c41f161
DAY0522 <ekdud_____@naver.com>
2024-01-27 17:47:13 +0900

    feat: add allowed host

--- bridge/bridge/settings.py
+++ bridge/bridge/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m!-ld1cg$l-pt@5a$90=^*vs6lop+@vp)zh1%arr@8bw)ketjy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
