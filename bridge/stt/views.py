2e9957476659688af6875ef591c18a0db5407dcb
DAY0522 <ekdud_____@naver.com>
2024-01-30 04:55:00 +0900

    Edit STT api

--- bridge/stt/views.py
+++ bridge/stt/views.py
@@ -31,35 +31,31 @@ def convert_aac_to_flac(audio_file):
 @csrf_exempt
 def speech_to_text(request):
     if request.method == 'POST':
-        try:
-            audio_file = request.FILES['audio']  # HTML form을 통해 업로드된 오디오 파일
-            
-            flac_file_path = convert_aac_to_flac(audio_file) # AAC 파일을 FLAC 파일로 변환
+        audio_file = request.FILES['audio']  # HTML form을 통해 업로드된 오디오 파일
+        
+        flac_file_path = convert_aac_to_flac(audio_file) # AAC 파일을 FLAC 파일로 변환
 
-            # Google Cloud Speech-to-Text 클라이언트 초기화
-            client = speech.SpeechClient()
+        # Google Cloud Speech-to-Text 클라이언트 초기화
+        client = speech.SpeechClient()
 
-            # FLAC 파일을 읽습니다.
-            with io.open(flac_file_path, 'rb') as audio_file:
-                content = audio_file.read()
+        # FLAC 파일을 읽습니다.
+        with io.open(flac_file_path, 'rb') as audio_file:
+            content = audio_file.read()
 
-            audio = speech.RecognitionAudio(content=content)
+        audio = speech.RecognitionAudio(content=content)
 
-            # 인식 설정을 정의합니다.
-            config = speech.RecognitionConfig(
-                encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
-                language_code='ko-KR'  # 원하는 언어 코드 설정
-            )
+        # 인식 설정을 정의합니다.
+        config = speech.RecognitionConfig(
+            encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
+            language_code='ko-KR'  # 원하는 언어 코드 설정
+        )
 
-            # Speech-to-Text API 호출
-            response = client.recognize(config=config, audio=audio)
+        # Speech-to-Text API 호출
+        response = client.recognize(config=config, audio=audio)
 
-            # 결과 처리
-            for result in response.results:
-                print('Transcript: {}'.format(result.alternatives[0].transcript))
-                return JsonResponse({'transcript': result.alternatives[0].transcript})
-
-        finally:
-            os.remove(flac_file_path)  # 변환 후 임시 FLAC 파일 삭제
+        # 결과 처리
+        for result in response.results:
+            print('Transcript: {}'.format(result.alternatives[0].transcript))
+            return JsonResponse({'transcript': result.alternatives[0].transcript})
 
     return JsonResponse({'error': 'Invalid request'}, status=400)
