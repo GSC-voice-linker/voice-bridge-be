07decb75a16b2ff08e8027c0dd6501d1e1c93295
DAY0522 <ekdud_____@naver.com>
2024-02-15 07:26:54 +0900

    no message

--- bridge/sign_translation/views.py
+++ bridge/sign_translation/views.py
@@ -28,91 +28,68 @@ model = load_model('./sign_translation/model/Modeltmp2.h5')
 
 colors = [(245,117,16), (117,245,16), (16,117,245),(200,103,27),(245,117,16), (117,245,16), (16,117,245),(245,117,16), (117,245,16), (16,117,245)]
 
-tmp = [] #(여기)
-threshold = 0.6 #(여기)
-
 project_id = "voice-bridge-412704"
 location = "asia-northeast3"
 temperature = 0.9
 
 def multiple_detection(image,model):
-    image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB) #Color convertion 
+    image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
     image.flags.writeable = False
     results = model.process(image)
     image.flags.writeable = True
     image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
     return image,results
     
-def draw_landmarks(image, results):
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) # Draw pose connections
-    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw left hand connections
-    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw right hand connect
-
-
 def draw_styled_landmarks(image, results):
-    # Draw pose connections
     mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
                              mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4), 
                              mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
                              ) 
-    # Draw left hand connections
     mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
                              mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4), 
                              mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
                              ) 
-    # Draw right hand connections  
     mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
                              mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4), 
                              mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
                              ) 
-    
+
 def extract_keypoints(results):
-    # Extract pose landmarks (landmarks 11 to 22)
     pose = np.array([[res.x, res.y, res.z, res.visibility] for i, res in enumerate(results.pose_landmarks.landmark) if 11 <= i + 1 <= 22]).flatten() if results.pose_landmarks else np.zeros(12*4)
-
-    # Extract left hand landmarks
     lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
-
-    # Extract right hand landmarks
     rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
-
-    # Concatenate and return the results
     return np.concatenate([pose, lh, rh])
 
-# def prob_viz(res, actions, input_frame, colors):
-#     output_frame = input_frame.copy()
-#     for num, prob in enumerate(res):
-#         cv2.rectangle(output_frame, (0,60+num*40), (int(prob*100), 90+num*40), colors[num], -1)
-#         cv2.putText(output_frame, actions[num], (0, 85+num*40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA) 
-#     return output_frame
+def prob_viz(res, actions, input_frame, colors):
+    output_frame = input_frame.copy()
+    for num, prob in enumerate(res):
+        cv2.rectangle(output_frame, (0,60+num*40), (int(prob*100), 90+num*40), colors[num], -1)
+        cv2.putText(output_frame, actions[num], (0, 85+num*40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA) 
+    return output_frame
 
 def translate_sign_lauguage(video_path):
     with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
 
-        # New code to load an mp4 file
+        tmp = [] #(여기)
+        threshold = 0.6 #(여기)
         cap = cv2.VideoCapture(video_path)
         sequence = []
         sentence = []
         predictions = []
-        tmp = [] #(여기)
-        threshold = 0.6 #(여기)
+        output = []
 
         while cap.isOpened():
-            # Read feed
             ret, frame = cap.read()
+            if not ret:
+                break
 
-            # Make detections
             image, results = multiple_detection(frame, holistic)
-            # print(results)
-            
-            # Draw landmarks
             draw_styled_landmarks(image, results)
-            
-            # 2. Prediction logic
+
             keypoints = extract_keypoints(results)
             sequence.append(keypoints)
             sequence = sequence[-35:]
-            
+
             if len(sequence) == 35:
                 res = model.predict(np.expand_dims(sequence, axis=0))[0]
                 # print(actions[np.argmax(res)])
@@ -121,7 +98,7 @@ def translate_sign_lauguage(video_path):
                 # print("unique predictions in 15 frame : ",np.unique(predictions[-15:]))
                 # print("max predictions in 15 frame    : ",np.unique(predictions[-15:])[0])
                 
-            #3. Viz logic(여기서 부터)
+                #3. Viz logic(여기서 부터)
                 if np.unique(predictions[-10:])[0]==np.argmax(res): 
                     if res[np.argmax(res)] > threshold: 
                         
@@ -132,12 +109,17 @@ def translate_sign_lauguage(video_path):
                                     if actions[np.argmax(res)] != sentence[-1]:
                                         sentence.append(actions[np.argmax(res)])
                                         tmp = []
-                            # print("tmp                            : ",tmp[-3:])
-                        else:
+                        else:                                                               
                             sentence.append(actions[np.argmax(res)])
 
                 if len(sentence) > 5: 
                     sentence = sentence[-1:]
+
+            if output == [] and sentence != "normal":
+                output.append(sentence)
+            elif output != [] and sentence != "normal":
+                if output[-1] != sentence:
+                    output.append(sentence)
 
             cv2.rectangle(image, (0,0), (640, 40), (245, 117, 16), -1)
             cv2.putText(image, ' '.join(sentence), (3,30), 
